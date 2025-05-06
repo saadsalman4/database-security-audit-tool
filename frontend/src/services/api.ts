e5f03b057b2f78db021fad71e617f55193eed66c
saadsalman4 <139686182+saadsalman4@users.noreply.github.com>
2025-05-07 00:44:07 +0500

    test deployment

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://wur7azpjs9.execute-api.us-east-1.amazonaws.com/dev',
   headers: {
     'Content-Type': 'application/json',
   },
