638c9dce9a0a66e2c4b0a49893e33d721f58c1c3
saadsalman4 <139686182+saadsalman4@users.noreply.github.com>
2025-05-06 23:34:45 +0500

    serverless v1

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const serverless = require("serverless-http");
+
 
 const app = express();
 const PORT = 3000;
@@ -13,10 +15,9 @@ app.use(express.json());
 
 app.use('/api', auditRoutes)
 
-app.listen(PORT, (error) =>{
-    if(!error)
-        console.log("Server is Successfully Running, and App is listening on port "+ PORT)
-    else 
-        console.log("Error occurred, server can't start", error);
-    }
-);
+app.get("/", (req, res) => {
+    res.send("Hello from Lambda!");
+  });
+  
+  // Export the handler
+  module.exports.handler = serverless(app);
